e6e6cea86dae2f448f608254b1a46b3267b6dc77
pkostua <pkostua@gmail.com>
2024-10-05 12:56:18 +0300

    Create locals.tf

---
+++ task06/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  vm_web_name = "${var.project_name}-${var.area_develop_name}-${var.resource_type_platform_name}-web"
+  vm_db_name = "${var.project_name}-${var.area_develop_name}-${var.resource_type_platform_name}-db"
+}
