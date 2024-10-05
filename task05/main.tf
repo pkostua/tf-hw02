a311c1145d188229620f976e0081aaa4ec929542
pkostua <pkostua@gmail.com>
2024-10-05 12:31:58 +0300

    Create main.tf

---
+++ task05/main.tf
@@ -0,0 +1,81 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "${var.vpc_name}_a"
+  zone           = var.zone_a
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.cidr_a
+}
+
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}_b"
+  zone           = var.zone_b
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.cidr_b
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_family_ubuntu
+}
+
+###web instance
+resource "yandex_compute_instance" "platform_web" {
+  name        = local.vm_web_name
+  platform_id = var.vm_platform_id_standard-v3
+  zone = var.zone_a
+  resources  {
+    cores = var.vm_web_resources.cores
+    memory = var.vm_web_resources.memory
+    core_fraction = var.vm_web_resources.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_a.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "${var.vms_ssh_user_name}:${var.vms_ssh_root_key}"
+  }
+
+}
+
+###db instance
+resource "yandex_compute_instance" "platform_db" {
+  name        = local.vm_db_name
+  platform_id = var.vm_platform_id_standard-v3
+  zone = var.zone_b
+  resources  {
+    cores = var.vm_db_resources.cores
+    memory = var.vm_db_resources.memory
+    core_fraction = var.vm_db_resources.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "${var.vms_ssh_user_name}:${var.vms_ssh_root_key}"
+  }
+
+}
