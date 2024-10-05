fc8086b60f5f7ff4ca1b696ef7dfca7363078be9
pkostua <pkostua@gmail.com>
2024-10-05 11:57:35 +0300

    Create variables.tf

---
+++ task03/variables.tf
@@ -0,0 +1,57 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "zone_a" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "cidr_a" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr_b" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqXhshMf6TVUZf0HnvXhwEnxFmA3Zvv6Fo57Ce/xbsq ubuntu@ubuntu2004"
+  description = "ssh-keygen -t ed25519"
+}
+
+variable "vms_ssh_user_name" {
+  type = string
+  default = "ubuntu"
+}
