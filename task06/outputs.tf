85e79eed288ac46e51b5bc6fb06c587e64071f2c
pkostua <pkostua@gmail.com>
2024-10-08 09:42:23 +0300

    Update outputs.tf

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -2,12 +2,12 @@ output "vm_outputs" {
   value = [
     {
       instance_name = yandex_compute_instance.platform_web.name
-      external_ip   = yandex_compute_instance.platform_web.network_interface[0].ip_address
+      external_ip   = yandex_compute_instance.platform_web.network_interface[0].nat_ip_address
       fqdn          = yandex_compute_instance.platform_web.fqdn
     },
     {
       instance_name = yandex_compute_instance.platform_db.name
-      external_ip   = yandex_compute_instance.platform_db.network_interface[0].ip_address
+      external_ip   = yandex_compute_instance.platform_db.network_interface[0].nat_ip_address
       fqdn          = yandex_compute_instance.platform_db.fqdn
     }
   ]
